68b3d7c08e2215ce775ab345a143e6a7f324ab55
mahtoaman <itismyacnt@gmail.com>
2023-01-26 13:38:04 +0530

    part3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,17 +2,14 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-import notes from '../notes'
-
-function getNotes(note){
-return <Note key={note.key} title={note.title} content={note.content} />;
-}
+import CreateArea from "./CreaterArea";
 
 function App() {
   return (
     <div>
       <Header />
-      {notes.map(getNotes)}
+      <CreateArea />
+      <Note key={1} title="Note title" content="Note content" />
       <Footer />
     </div>
   );
