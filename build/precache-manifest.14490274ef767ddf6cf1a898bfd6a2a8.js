27df534b0afe7c8c2e8af1c33907363768d26ea7
shreya <10shreyad@gmail.com>
2023-01-26 23:01:38 +0530

    build done

---
+++ build/precache-manifest.14490274ef767ddf6cf1a898bfd6a2a8.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3333d64a8e71b647e4d5bcf1c6e3905",
+    "url": "/index.html"
+  },
+  {
+    "revision": "218371ee1466ea6d2bca",
+    "url": "/static/js/2.fcc83c5d.chunk.js"
+  },
+  {
+    "revision": "517aaf2de4e0e08af084",
+    "url": "/static/js/main.2c57bb1b.chunk.js"
+  },
+  {
+    "revision": "325f4b8f32369198a2c5",
+    "url": "/static/js/runtime-main.4e550677.js"
+  }
+]);
